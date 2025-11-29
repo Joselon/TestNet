3d1ddb9ce133f3515acc3974c8782f64205b2e8b
Joselon <jose.navarro.osta@gmail.com>
2025-11-29 16:31:10 +0000

    Test SQL. Refactor with Task

--- TestNet/Program.cs
+++ TestNet/Program.cs
@@ -1,42 +1,45 @@
 ﻿//TestNet Program.cs
+using System.Data.SqlTypes;
 using Microsoft.Data.SqlClient;
 
 Console.WriteLine("TestNet init");
 
 using var con = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TestNetdB;Integrated Security=True;Connect Timeout=30");
-//con.Open();
 await con.OpenAsync();
 
-//Select
-using var selectPre = new SqlCommand("SELECT TOP 10 Id, Name FROM Users", con);
-var readerPre = await selectPre.ExecuteReaderAsync();
-Console.WriteLine("Initial Users:");
-while (await readerPre.ReadAsync())
-{
-    PrintUser((int)readerPre["Id"], readerPre["Name"].ToString());
-}
+//SelectPre
 
-readerPre.Close();
+Console.WriteLine("Initial Users:");
+await selectAllUsers(con);
+Console.WriteLine("\n--------------------------\n");
 
 //Insert
+Console.WriteLine("New User insertion:");
 using var insert = new SqlCommand("INSERT INTO Users (Name) VALUES ('NewUser'+ CAST(((SELECT COUNT(*) FROM Users) +1) AS NVARCHAR))", con);
 var rows = await insert.ExecuteNonQueryAsync();
 Console.WriteLine($"Inserted rows: {rows}");
+Console.WriteLine("\n--------------------------\n");
 
-//Select
-using var selectPost = new SqlCommand("SELECT TOP 10 * FROM Users", con);
-var readerPost = await selectPost.ExecuteReaderAsync();
-Console.WriteLine("Post-insert Users:");
-while (await readerPost.ReadAsync())
-{
-    PrintUser((int)readerPost["Id"], readerPost["Name"].ToString());
-}
-readerPost.Close();
-//con.Close();
+//SelectPost
+Console.WriteLine("All Users:");
+await selectAllUsers(con);
+Console.WriteLine("\n--------------------------\n");
+
 Console.WriteLine("TestNet end");
 
 static void PrintUser(int id, string? name)
 {
     Console.WriteLine("\n" + "Id -> " + id);
     Console.WriteLine("Name -> " + name + "\n");
+}
+
+async Task selectAllUsers(SqlConnection con)
+{
+    using var selectAll = new SqlCommand("SELECT TOP 10 Id, Name FROM Users", con);
+    var readerAll = await selectAll.ExecuteReaderAsync();
+    while (await readerAll.ReadAsync())
+    {
+        PrintUser((int)readerAll["Id"], readerAll["Name"].ToString());
+    }
+    readerAll.Close();
 }
